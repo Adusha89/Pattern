6a64d3eb918b57b125479e6f57f984304189f1ac
Andrey <belyakov.andrey.n@gmail.com>
2022-02-08 22:29:13 +0300

    Start 6

---
+++ Six/build/CMakeFiles/Six.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Six.dir/main.cpp.o"
+  "CMakeFiles/Six.dir/main.cpp.o.d"
+  "Six"
+  "Six.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Six.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
